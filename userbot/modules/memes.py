f1ac38cd8fdb5fbd20043ee976772988375436ae
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2019-09-27 19:25:18 +0530

    Fix decide meme

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -727,24 +727,16 @@ async def lol(lel):
 @errors_handler
 async def decide(event):
     decision = event.pattern_match.group(1).lower()
-    message_id = None
-    if event.reply_to_msg_id:
-        message_id = event.reply_to_msg_id
+    message_id = event.reply_to_msg_id if event.reply_to_msg_id else None
+    if decision != "decide":
+        r = requests.get(f"https://yesno.wtf/api?force={decision}").json()
     else:
-        try:
-            if decision != "decide":
-                r = requests.get(
-                    f"https://yesno.wtf/api?force={decision}").json()
-            else:
-                r = requests.get(f"https://yesno.wtf/api").json()
-            await event.client.send_message(event.chat_id,
-                                            str(r["answer"]).upper(),
-                                            reply_to=message_id,
-                                            file=r["image"])
-        except Exception as err:
-            await event.edit(f"`Error:` {str(err)}")
-            return
+        r = requests.get(f"https://yesno.wtf/api").json()
     await event.delete()
+    await event.client.send_message(event.chat_id,
+                                    str(r["answer"]).upper(),
+                                    reply_to=message_id,
+                                    file=r["image"])
 
 
 @register(outgoing=True, pattern="^;_;$")
